c815a45bdee93ed0ca4a22c9039c1b3b19d33033
zak905 <zakaria.amine88@gmail.com>
2017-08-27 18:17:10 +0200

    update test resource script url and polish

--- src/test/java/com/gwidgets/leaflet/test/InjectedLeafletResources.java
+++ src/test/java/com/gwidgets/leaflet/test/InjectedLeafletResources.java
@@ -7,24 +7,11 @@ import elemental2.dom.HTMLLinkElement;
 import elemental2.dom.HTMLScriptElement;
 
 public class InjectedLeafletResources {
-	
-
-public static void whenReady(OnloadCallbackFn function){
-		
+public static void whenReady(OnloadCallbackFn function){		
 		HTMLScriptElement leafletScript = (HTMLScriptElement) DomGlobal.document.createElement("script");
-		leafletScript.src="http://unpkg.com/leaflet@1.0.0/dist/leaflet.js";
+		leafletScript.src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/leaflet.js";
 		leafletScript.type="text/javascript";
-		
-		HTMLLinkElement leafletStyle = (HTMLLinkElement) DomGlobal.document.createElement("link");
-		leafletStyle.href="http://unpkg.com/leaflet@1.0.0/dist/leaflet.css";
-		leafletStyle.rel="stylesheet";
-		
-		
-		DomGlobal.document.head.appendChild(leafletScript);
-		DomGlobal.document.head.appendChild(leafletStyle);
-		
+		DomGlobal.document.head.appendChild(leafletScript);		
 		leafletScript.onload = function;
-
 	}
-
 }
