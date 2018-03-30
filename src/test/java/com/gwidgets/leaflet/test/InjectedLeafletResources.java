14430ea6eea01063652b1d38529205cf97cf3d5c
zak905 <zakaria.amine88@gmail.com>
2018-03-30 18:12:42 +0200

    upgrade elemental version

--- src/test/java/com/gwidgets/leaflet/test/InjectedLeafletResources.java
+++ src/test/java/com/gwidgets/leaflet/test/InjectedLeafletResources.java
@@ -2,12 +2,11 @@ package com.gwidgets.leaflet.test;
 
 
 import elemental2.dom.DomGlobal;
-import elemental2.dom.Element.OnloadFn;
-import elemental2.dom.HTMLLinkElement;
+import elemental2.dom.Element;
 import elemental2.dom.HTMLScriptElement;
 
 public class InjectedLeafletResources {
-public static void whenReady(OnloadFn function){		
+public static void whenReady(Element.OnloadFn function){
 		HTMLScriptElement leafletScript = (HTMLScriptElement) DomGlobal.document.createElement("script");
 		leafletScript.src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/leaflet.js";
 		leafletScript.type="text/javascript";
